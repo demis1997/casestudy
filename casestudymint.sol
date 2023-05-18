6b9b23ac918981291ca73111d12b0e2c52a173ee
demis1997 <demis.konstantinou16@gmail.com>
2023-05-19 01:25:28 +0300

    updated with openzeppelin libraries

--- casestudymint.sol
+++ casestudymint.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import 'oracle.js';
-contract TheRamenShopNFT is ERC721A, Ownable, ReentrancyGuard {
+contract caseStudy is ERC721A, Ownable, ReentrancyGuard {
 
   using Strings for uint256;
   bytes32 private commitment;
